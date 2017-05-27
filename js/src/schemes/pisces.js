9805f9c61c52d4760bc04eeb917b0db28ab35efb
kinglerzou <zoujian283827940@gmail.com>
2017-05-27 12:21:57 +0800

    Site updated: 2017-05-27 12:21:56

--- js/src/schemes/pisces.js
+++ js/src/schemes/pisces.js
@@ -1,3 +1,3 @@
-// build time:Fri May 26 2017 22:37:41 GMT+0800 (CST)
+// build time:Sat May 27 2017 12:21:45 GMT+0800 (CST)
 $(document).ready(function(){var n=$(".header-inner");var r=$("#sidebar");var e=function(){return n.height()+CONFIG.sidebar.offset};var i=function(n){return r.css({"margin-top":n})};var a=window.matchMedia("(min-width: 991px)");i(e()).show();a.addListener(function(n){if(n.matches){i(e())}})});
 //rebuild by neat 
