64188a217e61155c5d921e93dbcfe39607a09dbc
kinglerzou <zoujian283827940@gmail.com>
2017-06-08 20:37:30 +0800

    Site updated: 2017-06-08 20:37:30

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Thu Jun 08 2017 20:25:28 GMT+0800 (CST)
+// build time:Thu Jun 08 2017 20:37:11 GMT+0800 (CST)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
