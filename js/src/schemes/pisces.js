3dde7f312fbe15c0d1b27766803df3762a555bcc
kinglerzou <zoujian283827940@gmail.com>
2017-06-14 22:14:13 +0800

    Site updated: 2017-06-14 22:14:12

--- js/src/schemes/pisces.js
+++ js/src/schemes/pisces.js
@@ -1,3 +1,3 @@
-// build time:Mon Jun 12 2017 22:31:39 GMT+0800 (CST)
+// build time:Wed Jun 14 2017 22:13:54 GMT+0800 (CST)
 $(document).ready(function(){var n=$(".header-inner");var r=$("#sidebar");var e=function(){return n.height()+CONFIG.sidebar.offset};var i=function(n){return r.css({"margin-top":n})};var a=window.matchMedia("(min-width: 991px)");i(e()).show();a.addListener(function(n){if(n.matches){i(e())}})});
 //rebuild by neat 
